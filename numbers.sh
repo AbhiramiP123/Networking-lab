6b70c0280ab0d3cb48f04159653fac544a19d6b1
Abhirami p kumar_kinghuzzzz <97221313+AbhiramiP123@users.noreply.github.com>
2022-05-05 15:14:46 +0530

    using switch case

---
+++ numbers.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo "Enter the numbers:"
+read numbers
+
+case $numbers in
+        "1")
+        echo "print one".
+        echo
+        ;;
+        "2")
+        echo "print two".
+        echo
+        ;;
+        "3")
+        echo "print three".
+        echo
+        ;;
+        "4")
+        echo "print four".
+        echo
+        ;;
+        "5")
+        echo "print five".
+        echo
+        ;;
+        "6")
+        echo "print six".
+        echo
+        ;;
+        "7")
+        echo "print seven".
+        echo
+        ;;
+        "8")
+        echo "print eight".
+        echo
+        ;;
+        "9")
+        echo "print nine".
+        echo
+        ;;
+        "10")
+        echo "print ten".
+        echo
+        ;;
+        *)
+        echo "you discovered an unknown numbers".
+        echo
+        ;;
+esac
